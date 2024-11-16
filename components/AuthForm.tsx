5b8e7b3228ed10e6481fc4e72de5a0a987c20908
amritcoder <amrit2003btech@gmail.com>
2024-11-17 01:03:35 +0530

    first commit

---
+++ components/AuthForm.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+type FormType = 'sign-in' | 'sign-up'
+
+const AuthForm = ({type}: {type: FormType}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AuthForm
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,113 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			brand: {
+  				'100': '#BFDBFE',
+  				DEFAULT: '#0EA5E9'
+  			},
+  			red: '#FF7474',
+  			error: '#b80000',
+  			green: '#3DD9B3',
+  			blue: {
+          '500': '#0EA5E9', // Primary Sky Blue
+          '700': '#0369A1' // Secondary Deep Blue
+        },
+  			pink: '#EEA8FD',
+  			orange: '#F9AB72',
+  			light: {
+  				'100': '#333F4E',
+  				'200': '#A3B2C7',
+  				'300': '#F2F5F9',
+  				'400': '#F2F4F8'
+  			},
+        gray: {
+          '50': '#F5F5F5', // Background Soft White
+          '300': '#E0E0E0', // Accent Light Gray
+          '800': '#333333' // Text Charcoal Gray
+        },
+  			dark: {
+  				'100': '#04050C',
+  				'200': '#131524'
+  			},
+        background: '#F5F5F5', // Background color
+        
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+          DEFAULT: '#0EA5E9', // Sky Blue
+          foreground: '#FFFFFF' // White for text on primary
+        },
+        secondary: {
+          DEFAULT: '#0369A1', // Deep Blue
+          foreground: '#FFFFFF' // White for text on secondary
+        },
+  			muted: {
+          DEFAULT: '#E0E0E0', // Light Gray
+          foreground: '#333333' // Dark Gray text on muted background
+        },
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		fontFamily: {
+  			poppins: ["var(--font-poppins)"]
+  		},
+  		boxShadow: {
+  			'drop-1': '0px 10px 30px 0px rgba(66, 71, 97, 0.1)',
+  			'drop-2': '0 8px 30px 0 rgba(65, 89, 214, 0.3)',
+  			'drop-3': '0 8px 30px 0 rgba(65, 89, 214, 0.1)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'caret-blink': {
+  				'0%,70%,100%': {
+  					opacity: '1'
+  				},
+  				'20%,50%': {
+  					opacity: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'caret-blink': 'caret-blink 1.25s ease-out infinite'
+  		}
+  	}
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [require("tailwindcss-animate")],
+};
+export default config;
